08181b56230cdf7888977d83f7b34a6092ca7c3f
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-04-03 17:03:28 +0300

    start hw

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
      Hello, samurai! Let's go!
+        <ul>
+            <li>html</li>
+            <li>css</li>
+            <li>js</li>
+            <li>react</li>
+        </ul>
     </div>
   );
 }
