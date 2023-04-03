08f12c29218671b45f4fc9e09431be3a070fba86
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-04-03 20:46:06 +0300

    add solution lesson 7

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,36 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-        <ul>
-            <li>html</li>
-            <li>css</li>
-            <li>js</li>
-            <li>react</li>
-        </ul>
-    </div>
-  );
+    return (
+        <div>
+            Hello, samurai! Let's go!
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <div>
+            <a href='#'>Home</a>
+            <a href='#'>News Feed</a>
+            <a href='#'>Messages</a>
+        </div>
+    )
+}
+
+function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
