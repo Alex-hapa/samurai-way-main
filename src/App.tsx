b07caa4c6e1c1e0d259c6483beabd9260df201a5
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-04-06 17:52:43 +0300

    add html and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
 
-function App() {
+
+const App = () => {
     return (
-        <div>
-            Hello, samurai! Let's go!
-            <Header/>
-            <Technologies/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img
+                    src="https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr"
+                    alt="image"/>
+            </header>
+            <nav className='nav'>
+                <div><a href="#">Profile</a></div>
+                <div><a href="#">Messages</a></div>
+                <div><a href="#">News</a></div>
+                <div><a href="#">Music</a></div>
+                <div><a href="#">Settings</a></div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img
+                        src="https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w="
+                        alt="image"/>
+                </div>
+                <div>
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoAEssp5xBCP420VJTAkFru3VTpDnHVnXzjg&usqp=CAU"
+                        alt="image"/>
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>post1</div>
+                        <div>post2</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
