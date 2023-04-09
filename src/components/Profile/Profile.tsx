26c761195313a2d4c7352a35282a35a5a693fb1b
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-04-10 00:32:58 +0300

    add Routing between Dialogs and Profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={cl.content}>
+        <div>
             <div>
                 <img
                     src="https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w="
